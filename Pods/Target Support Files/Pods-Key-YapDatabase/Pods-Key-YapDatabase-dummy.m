7f476a4dac3e4a1b0a2e4be01e61124330e0970a
Brendan Farmer <bpfarmer@gmail.com>
2015-02-23 14:23:15 -0500

    removing gitignored files

--- Pods/Target Support Files/Pods-Key-YapDatabase/Pods-Key-YapDatabase-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Key_YapDatabase : NSObject
-@end
-@implementation PodsDummy_Pods_Key_YapDatabase
-@end
--- Pods/Target Support Files/Pods-KeyTests-SSKeychain/Pods-KeyTests-SSKeychain-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_KeyTests_SSKeychain : NSObject
-@end
-@implementation PodsDummy_Pods_KeyTests_SSKeychain
-@end
